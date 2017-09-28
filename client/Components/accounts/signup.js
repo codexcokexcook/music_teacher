50564def04c59f54e5fa9c0d1428a887d3a723f8
codexcokexcook <32152882+codexcokexcook@users.noreply.github.com>
2017-09-28 14:28:26 +0800

    updated the package

--- client/Components/accounts/signup.js
+++ client/Components/accounts/signup.js
@@ -79,7 +79,7 @@ Template.signup_content.events({
           password: password,
           profile:{
           first_name: first_name,
-          last_name: last_name
+          last_name: last_name,
         }
       }, function(err){
         if(err){
--- server/signup.js
+++ server/signup.js
@@ -2,7 +2,10 @@ import { Meteor } from 'meteor/meteor';
 import { Email } from 'meteor/email'
 
 Accounts.onCreateUser(function(options, user) {
-    user.profile = {};
+    user.profile = {
+      
+
+    };
 
     // we wait for Meteor to create the user before sending an email
     Meteor.setTimeout(function() {
