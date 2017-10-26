81dd6d31b61866baefae07569271528405a5e8d6
codexcokexcook <32152882+codexcokexcook@users.noreply.github.com>
2017-10-26 17:59:44 +0800

    Profile Created and Profile Card Modification

--- server/components/create_profile.js
+++ server/components/create_profile.js
@@ -31,26 +31,36 @@ Meteor.methods({
 
   'profile_details.insert'(
     user_id,
-    first_name,
-    last_name,
-    kitchen_name,
+    mobile,
     profile_keywords,
     date_of_birth,
     gender,
     about_myself,
     allergy_tags,
-    dietary_tags  ){
+    dietary_tags,
+    home_address,
+    office_address,
+    card_number,
+    card_fullname,
+    card_exp_month,
+    card_exp_year,
+    cvv_code ){
     Profile_details.insert({
-    user_id,
-    first_name: first_name,
-    last_name: last_name,
-    kitchen_name: kitchen_name,
+    user_id: user_id,
+    mobile: mobile,
     profile_keywords: profile_keywords,
     date_of_birth: date_of_birth,
     gender: gender,
     about_myself: about_myself,
     allergy_tags: allergy_tags,
-    dietary_tags: dietary_tags
+    dietary_tags: dietary_tags,
+    home_address: home_address,
+    office_address: office_address,
+    card_number: card_number,
+    card_fullname: card_fullname,
+    card_exp_month: card_exp_month,
+    card_exp_year: card_exp_year,
+    cvv_code: cvv_code
 
   });
 },
