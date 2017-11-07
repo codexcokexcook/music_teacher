82c19d3aa35246900703a68179e2c70c0f6c253c
codexcokexcook <32152882+codexcokexcook@users.noreply.github.com>
2017-11-08 03:35:17 +0800

    Shopping Cart & navbar
    dish_list.js for the calculation of the order
    navbar.html for the modification of the navbar
    navbar.js for the calculation of the shopping cart
    shopping_cart.css, html & js for the shopping cart (Not Completed)
    route.js. shopping cart page
    create_dishes_form.js - remove shopping cart item

--- client/Components/shopping_cart/shopping_cart.js
+++ client/Components/shopping_cart/shopping_cart.js
@@ -13,26 +13,45 @@ Template.shopping_cart_card.onRendered(function(){
 Template.shopping_cart_card.helpers({
 
 'shopping_cart': function(){
-
-  var shopping_cart = Shopping_cart.find({"buyer_id": Meteor.userId()})
-  return shopping_cart
+    var shopping_cart = Shopping_cart.find({"buyer_id": Meteor.userId()})
+    return shopping_cart
 },
 
 'total_price_per_dish': function(){
     var total_price_per_dish = this.quantity*this.product_price
-    $('')
+    return total_price_per_dish
 
   }
 
 })
 
+Template.sc_payment.helpers({
+  'get_credit_card': function(){
+      var profile_details = Profile_details.findOne({"user_id": Meteor.userId()})
+      return profile_details.card_number
+  }
+})
+
+
 
 Template.shopping_cart_card.events({
-  'change .quantity': function(event){
-    var quantity = document.getElementById(this._id).value;
+'change .quantity': function(event){
+    var cart_id = this._id
+    var field_name = this._id+"_quantity"
+    var quantity = document.getElementById(field_name).value;
 
+    Meteor.call('shopping_cart.update',
+      cart_id,
+      quantity)
+    },
 
-},
+'click .remove_button': function(event){
+    var cart_id = this._id;
+
+    Meteor.call('shopping_cart.remove',
+      cart_id)
+  }
+
 })
 
 /**
