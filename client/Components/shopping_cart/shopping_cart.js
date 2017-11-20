8b2cb506c18cf6c99cf3711f422242c20c682443
codexcokexcook <thomas.lee@blueplate.co>
2017-11-20 12:29:11 +0800

    Shopping cart

--- client/Components/shopping_cart/shopping_cart.js
+++ client/Components/shopping_cart/shopping_cart.js
@@ -6,8 +6,17 @@ import { FilesCollection } from 'meteor/ostrio:files';
 import { search_distinct } from '/imports/functions/find_by.js'
 
 
-Template.shopping_cart_card.onRendered(function(){
-
+Template.sc_serving_details.onRendered(function(){
+  this.$('.dropdown-button').dropdown({
+     inDuration: 300,
+     outDuration: 225,
+     constrainWidth: false, // Does not change width of dropdown to that of the activator
+     hover: false, // Activate on hover
+     gutter: 0, // Spacing from edge
+     belowOrigin: true, // Displays dropdown below the button
+     alignment: 'left', // Displays dropdown with edge aligned to the left of button
+     stopPropagation: true // Stops event propagation
+   })
 
 });
 
