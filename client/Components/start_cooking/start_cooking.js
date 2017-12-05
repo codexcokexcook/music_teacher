ea423a1c74432f37a9bbc44004698948833b8b67
codexcokexcook <thomas.lee@blueplate.co>
2017-12-05 15:51:27 +0800

    start cooking - receiving orders

--- client/Components/start_cooking/start_cooking.js
+++ client/Components/start_cooking/start_cooking.js
@@ -3,15 +3,58 @@ import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import { Template } from 'meteor/templating';
 import { Blaze } from 'meteor/blaze';
 import { FilesCollection } from 'meteor/ostrio:files';
-
+import { search_distinct_in_order_record } from '/imports/functions/shopping_cart.js';
 
 Template.start_cooking.helpers({
 
   'order': function(){
-    return Order_record.find({seller_id: Meteor.userId(), status: 'Created'})
+    var order = Order_record.find({'seller_id': Meteor.userId(), 'status': 'Created'}).count()
+    console.log(order)
+    return order
+
+  },
+
+  'order_received': function(){
+
+ var check = search_distinct_in_order_record('buyer_id')
+    console.log(check)
+
+     function find_in_order_record(array_value){
+       var buyer_id = array_value
+       console.log(buyer_id)
+       var result = Order_record.find({'buyer_id': buyer_id, 'seller_id': Meteor.userId(), 'status': 'Created'})
+       console.log(result)
+       return result
+
+     }
+
+     for(i=0;i<check.length;i++){
+       var result =[]
+      result[i] = find_in_order_record(check[i])
+     }
+     console.log(result)
+     return result
+},
+
+
+
+})
+
+
+
+Template.request_card.helpers({
+
+  'foodie_profile_picture': function(){
+    console.log(this.buyer_id)
+    var foodie = profile_images.findOne({'userId': this.buyer_id, "meta": {"purpose": "profile_picture"}})
+    console.log(foodie._id + foodie.extensionWithDot)
+    return foodie._id + foodie.extensionWithDot
+
+  },
+
+  'order_dish': function(){
+
   }
-
-
 
 
 })
--- imports/functions/shopping_cart.js
+++ imports/functions/shopping_cart.js
@@ -19,10 +19,16 @@ export function search_distinct_for_delivery_in_shopping_cart(field){
       }).fetch().map(x => x[field]), true);
     }
 
+    export function search_distinct_in_order_record(field){
+      return _.uniq(Order_record.find({'seller_id': Meteor.userId(), 'status':'Created'}, {
+          sort: {[field]: 1}, fields: {[field]: 1}
+        }).fetch().map(x => x[field]), true);
+      }
+
+/** Search Distinct Originial
   export function search_distinct(collection, field){
     return _.uniq(collection.find({'buyer_id': Meteor.userId()}, {
         sort: {[field]: 1}, fields: {[field]: 1}
       }).fetch().map(x => x[field]), true);
     }
-
-  
+**/
