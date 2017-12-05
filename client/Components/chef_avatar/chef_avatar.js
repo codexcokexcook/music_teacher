edb8e4bac1eba54dea53302ad28828b58429dd31
blueplate-michaellin <michael.lin@blueplate.co>
2017-12-05 11:39:44 +0800

    Michael_041217
    completed responsive pinterest-like layout on showroom page.

--- client/Components/chef_avatar/chef_avatar.js
+++ client/Components/chef_avatar/chef_avatar.js
@@ -3,7 +3,7 @@ Template.chef_avatar.helpers({
     var returned_dish_id = Session.get('dish_id');
     var get_dish_details = Dishes.findOne({"_id":returned_dish_id});
     var get_dish_creator = get_dish_details && get_dish_details.user_id;
-    var get_profile_images = profile_images.findOne({'userId': get_dish_creator});
+    var get_profile_images = profile_images.findOne({'userId': get_dish_creator, 'meta':{"purpose": 'homecook_profile_picture'}});
     var get_profile_images_id = get_profile_images && get_profile_images._id;
     var get_profile_images_ext = get_profile_images && get_profile_images.extensionWithDot;
     var get_profile_images_name = get_profile_images_id + get_profile_images_ext;
