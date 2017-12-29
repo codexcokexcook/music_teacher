a7ced783394430a15cbbc954a92f911c0ea1be22
Tan Pham <tan.pham@blueplate.co>
2017-12-29 15:27:57 +0700

    Update kitchen_profile in order modal

--- client/Components/chef_avatar/chef_avatar.js
+++ client/Components/chef_avatar/chef_avatar.js
@@ -3,7 +3,7 @@ Template.chef_avatar.helpers({
     var returned_dish_id = Session.get('dish_id');
     var get_dish_details = Dishes.findOne({"_id":returned_dish_id});
     var get_dish_creator = get_dish_details && get_dish_details.user_id;
-    var get_profile_images = profile_images.findOne({'userId': get_dish_creator, 'meta':{"purpose": 'homecook_profile_picture'}});
+    var get_profile_images = profile_images.findOne({'userId': get_dish_creator, 'meta.purpose': 'homecook_profile_picture'});
     return get_profile_images.meta.base64;
   }
 });
