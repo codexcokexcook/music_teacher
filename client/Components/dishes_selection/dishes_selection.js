54ae48df095aa24dcdd8c80bc1b830d820d7f488
blueplate-michaellin <michael.lin@blueplate.co>
2018-01-21 13:32:56 +0800

    21 Jan 2018 - Michael
    fixed menu creation and edit issue

--- client/Components/dishes_selection/dishes_selection.js
+++ client/Components/dishes_selection/dishes_selection.js
@@ -4,7 +4,8 @@ Template.dishes_selection.onRendered(function(){
 
 Template.dishes_selection.events({
   'change .dishes_checkbox': function(event, template) {
-    var checked_dishes = template.findAll("input[type=checkbox]:checked");
+    var checked_dishes = template.findAll("input[class=dishes_checkbox]:checked");
+    console.log(checked_dishes)
     var checked_values = checked_dishes.map(function(selection){
     return selection.value;
   });
