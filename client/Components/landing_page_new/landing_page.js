b89c7f9828e4b1238ed9c9eb65870be4ccb16a55
tanpham <tan.pham@blueplate.co>
2018-03-12 18:26:35 +0700

    add pre-loader for landingpage when load

--- client/Components/landing_page_new/landing_page.js
+++ client/Components/landing_page_new/landing_page.js
@@ -6,14 +6,11 @@ import { Blaze } from 'meteor/blaze';
 import './landing_page.html';
 
 Template.landing_page.onRendered(function() {
-  $('.modal').modal({
-      dismissible: false, // Modal can be dismissed by clicking outside of the modal
-      opacity: .5, // Opacity of modal background
-      inDuration: 300, // Transition in duration
-      outDuration: 200, // Transition out duration
-      startingTop: '4%', // Starting top style attribute
-      endingTop: '10%', // Ending top style attribute
-    });
+  $('body').css('overflow-y', 'hidden');
+  window.onload = function(){
+    $('body').css('overflow-y', 'scroll');
+    $('.loader-wrapper').fadeOut('slow');
+  }
 });
 
 Template.login_modal.events({
