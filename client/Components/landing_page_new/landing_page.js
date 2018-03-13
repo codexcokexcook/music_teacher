32e7ac53fb72fd2d0a66d6bf694dcaa22faac653
tanpham <tan.pham@blueplate.co>
2018-03-13 17:50:19 +0700

    add basic animation to landing page

--- client/Components/landing_page_new/landing_page.js
+++ client/Components/landing_page_new/landing_page.js
@@ -1,21 +1,46 @@
-import { Accounts } from 'meteor/accounts-base';
-import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
-import { Template } from 'meteor/templating';
-import { Blaze } from 'meteor/blaze';
+import {
+  Accounts
+} from 'meteor/accounts-base';
+import {
+  FlowRouter
+} from 'meteor/ostrio:flow-router-extra';
+import {
+  Template
+} from 'meteor/templating';
+import {
+  Blaze
+} from 'meteor/blaze';
 
 import './landing_page.html';
 
-Template.landing_page.onRendered(function() {
+Template.landing_page.onRendered(function () {
   $('body').css('overflow-y', 'hidden');
-  window.onload = function(){
-    $('body').css('overflow-y', 'scroll');
-    $(document).scrollTop(0);
-    $('.loader-wrapper').fadeOut('slow');
+  // everything is loaded
+  window.onload = function () {
+    setTimeout(() => {
+      $('body').css('overflow-y', 'scroll');
+      $(document).scrollTop(0);
+      $('.loader-wrapper').fadeOut('slow');
+      $('.slogan').removeClass('notloaded');
+
+      if (!window.detectmob) {
+        // $('.what_happen').hide();
+        // $('.why_is_there').hide();
+        // $('.why_is_this').hide();
+        // $('.how_can_we').hide();
+        // $('.join_us').hide();
+      }
+
+      $('.vet_photo .changing').click(function(){
+        alert('asfhsdjfg');
+      });
+
+    }, 1000);
   }
 });
 
 Template.login_modal.events({
-  'click #login_modal_button': function() {
+  'click #login_modal_button': function () {
     if (login_content) {
       $('.forgot_password').remove();
       $('#login_modal').append(login_content);
@@ -23,6 +48,6 @@ Template.login_modal.events({
   }
 });
 
-Template.registerHelper('isCurrentUser',function(){
+Template.registerHelper('isCurrentUser', function () {
   return (localStorage.getItem("loggedIn").toLowerCase() === 'true');
 });
--- client/main.js
+++ client/main.js
@@ -1,27 +1,49 @@
-import { Accounts } from 'meteor/accounts-base'
+import {
+  Accounts
+} from 'meteor/accounts-base'
 
-Meteor.startup(function() {
+Meteor.startup(function () {
   Notification.requestPermission()
 
   GoogleMaps.load({
     key: 'AIzaSyBxRWAwnS9h8pP1mF6sAa4ZnkqGYUPBGac'
   });
 
-  window.fbAsyncInit = function() {
+  window.fbAsyncInit = function () {
     FB.init({
-      appId      : '{your-app-id}',
-      cookie     : true,
-      xfbml      : true,
-      version    : '{latest-api-version}'
+      appId: '{your-app-id}',
+      cookie: true,
+      xfbml: true,
+      version: '{latest-api-version}'
     });
     FB.AppEvents.logPageView();
   };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+
+  // check mobile device
+  window.detectmob = function () {
+    if (navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/webOS/i) ||
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/iPod/i) ||
+      navigator.userAgent.match(/BlackBerry/i) ||
+      navigator.userAgent.match(/Windows Phone/i)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  (function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+      return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 })
