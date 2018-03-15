9578ba7a437b920ac7ad73cb2cac9cf7b8d62d5a
tanpham <tan.pham@blueplate.co>
2018-03-15 22:36:18 +0700

    re-style star for rating

--- client/imports/ui/rating.js
+++ client/imports/ui/rating.js
@@ -13,11 +13,11 @@ export default class Rating extends Component {
   render() {
     return (
         <span className="rating-container">
-            <span className={ (this.state.rating >= 1) ? "fa fa-star checked " : "fa fa-star" }></span>
-            <span className={ (this.state.rating >= 2) ? "fa fa-star checked " : "fa fa-star" }></span>
-            <span className={ (this.state.rating >= 3) ? "fa fa-star checked " : "fa fa-star" }></span>
-            <span className={ (this.state.rating >= 4) ? "fa fa-star checked " : "fa fa-star" }></span>
-            <span className={ (this.state.rating >= 5) ? "fa fa-star checked " : "fa fa-star" }></span>
+            <span className={ (this.props.rating >= 1) ? "fa fa-star checked " : "fa fa-star empty" }></span>
+            <span className={ (this.props.rating >= 2) ? "fa fa-star checked " : "fa fa-star empty" }></span>
+            <span className={ (this.props.rating >= 3) ? "fa fa-star checked " : "fa fa-star empty" }></span>
+            <span className={ (this.props.rating >= 4) ? "fa fa-star checked " : "fa fa-star empty" }></span>
+            <span className={ (this.props.rating >= 5) ? "fa fa-star checked " : "fa fa-star empty" }></span>
         </span>
     );
   }
