d3110e9c63ae924d8ef469985d51e7abed01e387
tanpham <tan.pham@blueplate.co>
2018-03-22 11:00:07 +0700

    get location info from location address on server side

--- server/components/searching.js
+++ server/components/searching.js
@@ -5,10 +5,16 @@ import {
 import {
     HTTP
 } from 'meteor/http'
-import { console } from 'meteor/tools';
 
 Meteor.methods({
     'searching' (location, service, date, time) {
-        
+        HTTP.call( 'GET', 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyBxRWAwnS9h8pP1mF6sAa4ZnkqGYUPBGac', function( error, response ) {
+            // Handle the error or response here.
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(response);
+            }
+        });
     }
 });
