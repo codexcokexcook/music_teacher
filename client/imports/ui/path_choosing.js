b1e32192d2fb1f1fe24e73491ec3e2718fae7847
blueplate-michaellin <michael.lin@blueplate.co>
2018-03-22 14:37:17 +0700

    update path choosing

--- client/imports/ui/path_choosing.js
+++ client/imports/ui/path_choosing.js
@@ -10,12 +10,19 @@ export default class PathOption extends Component {
       {
         link: "https://s3-ap-southeast-1.amazonaws.com/blueplate-images/icons/BeHomecook.svg",
         name: "create your first dish",
+        details: "Create your first dish at blueplate",
+        redirect: "/cooking/dishes",
       }, {
         link: "https://s3-ap-southeast-1.amazonaws.com/blueplate-images/icons/ProfilePreview.svg",
         name: "preview kitchen profile",
+        details: "Have a quick look at your own profile. This is how other users are going to see you",
+        redirect: "",
       }, {
         link: "https://s3-ap-southeast-1.amazonaws.com/blueplate-images/icons/Dashboard.svg",
         name: "go to kitchen dashboard",
+        details: "The dashboard is the main page of your kitchen that contains all information you \
+        need to run your kitchen at blueplate",
+        redirect: "/cooking/dashboard",
       }
     ];
   }
@@ -26,9 +33,10 @@ export default class PathOption extends Component {
         <div className='col xl4 l4 m12 s12'>
           <div className='card z-depth-0 path_card'>
             <div className='card-content'>
-              <a>
+              <a href={path.redirect}>
                 <img src={path.link} className="path_icons"/>
                 <h6 className='icon_titles bp-blue-text center-align' id='create_first_dish'>{path.name}</h6>
+                <p className='bp-blue-text'>{path.details}</p>
               </a>
             </div>
           </div>
@@ -39,7 +47,15 @@ export default class PathOption extends Component {
 
   render() {
     return (
-      <div className="path_container valign-wrapper">
+      <div className="path_container">
+        <div className="row center-align">
+          <div className="col xl12 l12 m12 s12">
+            <h5 className ="bp-blue-text">
+              Thanks for taking your time filling in your profile.
+              All is done. Please choose where you would like to go.
+            </h5>
+          </div>
+        </div>
         <div className="row center-align">
           {this.render_path()}
         </div>
